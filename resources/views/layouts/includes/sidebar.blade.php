448adc5f4c9314f0e7020ddcccadd9bd54f94add
chinhvc <chinhvc93@gmail.com>
2019-01-02 00:14:52 +0900

    change name project

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -6,7 +6,7 @@
   -->
     <div class="logo">
         <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-            Thời tiết
+            Project 3
         </a>
     </div>
     <div class="sidebar-wrapper">
